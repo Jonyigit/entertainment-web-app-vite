9b0e01443a941777660c91ef8887d83ed0f1dfbc
Jonyigit <jonyigitavazbekov@gmail.com>
2025-05-24 18:16:39 +0300

    Download Swiper And Create MouseChange Slider..)

--- src/components/shared/Slider/Slider.jsx
+++ src/components/shared/Slider/Slider.jsx
@@ -4,12 +4,14 @@ import { useQuery } from "@tanstack/react-query";
 import { apiClient } from "../../../api";
 import SkeletonSliderCard from "../../ui/SliderCard/components/SkeletonSliderCard/SkeletonSliderCard";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel, Keyboard } from "swiper/modules";
+import "swiper/css";
+
 export default function Slider() {
     const { data, isPending } = useQuery({
         queryKey: ["trendingCinema"],
-        queryFn: () => {
-            return apiClient.get("api/movies");
-        },
+        queryFn: () => apiClient.get("api/movies"),
     });
 
     const trendingCinema = data?.data?.filter((item) => item.isTrending) || [];
@@ -17,26 +19,32 @@ export default function Slider() {
     return (
         <div className="slider-container">
             <h1>Trending</h1>
-            <div className="carrousel">
-                {isPending
-                    ? Array.from({ length: 3 }).map((_, i) => <SkeletonSliderCard key={i} />)
-                    : trendingCinema.map((item) => {
-                          let { id, title, category, rating, year, thumbnail, isBookmarked } = item;
-
-                          return (
-                              <SliderCard
-                                  key={id}
-                                  title={title}
-                                  category={category}
-                                  rating={rating}
-                                  year={year}
-                                  img={thumbnail.regular.large}
-                                  isBookmarked={isBookmarked}
-                                  id={id}
-                              />
-                          );
-                      })}
-            </div>
+            {isPending ? (
+                <div className="carrousel">
+                    {Array.from({ length: 3 }).map((_, i) => (
+                        <SkeletonSliderCard key={i} />
+                    ))}
+                </div>
+            ) : (
+                <Swiper modules={[Mousewheel, Keyboard]} slidesPerView="auto" spaceBetween={24} mousewheel keyboard>
+                    {trendingCinema.map((item) => {
+                        let { id, title, category, rating, year, thumbnail, isBookmarked } = item;
+                        return (
+                            <SwiperSlide key={id}>
+                                <SliderCard
+                                    title={title}
+                                    category={category}
+                                    rating={rating}
+                                    year={year}
+                                    img={thumbnail.regular.large}
+                                    isBookmarked={isBookmarked}
+                                    id={id}
+                                />
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
+            )}
         </div>
     );
 }
